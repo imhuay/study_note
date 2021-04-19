952a8eaf9e413cfb8465a9cce1e4285770ef3149
imhuay <imhuay@163.com>
2021-04-19 11:24:00 +0800

    更新 push 脚本

--- git-push.sh
+++ git-push.sh
@@ -3,6 +3,27 @@
 # git subtree add --prefix=code/keras_demo/keras_model/bert_by_keras bert_by_keras master --squash
 # git subtree add --prefix=code/keras_demo keras_demo master --squash
 
-git subtree push --prefix=code/keras_demo/keras_model/bert_by_keras bert_by_keras master
-git subtree push --prefix=code/keras_demo keras_demo master
+# 使用 submodule 代替 subtree
+# git subtree push --prefix=code/keras_demo/keras_model/bert_by_keras bert_by_keras master
+# git subtree push --prefix=code/keras_demo keras_demo master
+
+# 获取仓库父目录
+pwd=$(pwd)
+
+# 先更新子仓库
+printf "=== Update submodule first ===\n"
+printf "___ Start update bert_by_keras\n"
+cd "$pwd/code/my_models/bert_by_keras" || exit
+git pull origin master
+
+printf "\n=== Submodule status list ===\n"
+git submodule status
+echo
+
+# 更新父仓库
+cd "$pwd" || exit
+printf "\n=== Start father repository ===\n"
 git push
+
+
+
