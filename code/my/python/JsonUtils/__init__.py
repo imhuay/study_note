4c7e1139df365648a13fd96a68c5014319cacabb
imhuay <imhuay@163.com>
2021-06-24 00:06:34 +0800

    U: 重构

---
+++ code/my/python/JsonUtils/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+Time:
+    2021-06-23 11:54 下午
+    
+Author:
+    huayang
+    
+Subject:
+    
+"""
+
+from ._no_indent_encoder import NoIndentEncoder
